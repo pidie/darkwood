2457d545cf932bb0f50a0ea7173e3a89bc1bec62
pidie <81444291+pidie@users.noreply.github.com>
2023-10-05 14:57:20 -0400

    Update PhysicsInteractable.cs

--- Assets/Scripts/Physics_/PhysicsInteractable.cs
+++ Assets/Scripts/Physics_/PhysicsInteractable.cs
@@ -5,8 +5,15 @@ public class PhysicsInteractable : MonoBehaviour, IPhysicsInteractable
     [Header("Behaviors")]
     [SerializeField] private UnityEvent standardBehavior;
 
+    // add additional fields for different conditional behaviors here
+
+    private void OnEnable() => PhysicsManager.Instance.OnApplyPhysics += PhysicsBehavior;
+
+    private void OnDisable() => PhysicsManager.Instance.OnApplyPhysics -= PhysicsBehavior;
+
     private bool PhysicsBehavior()
     {
+        // as conditional behaviors are added, check for conditions before invoking methods
         standardBehavior?.Invoke();
     }
 }
